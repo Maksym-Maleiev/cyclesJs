175615df8544368c9c5f3b936ba997a863c5f561
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-11-11 14:27:21 +0200

    Update cyclesJs.js

--- cyclesJs.js
+++ cyclesJs.js
@@ -2,6 +2,6 @@
 var sheepCounted /* овець порахував */ = 0; 
 while (sheepCounted < 10) {
   console.log("I have conted " + sheepCounted + " sheep!");
-  sheepCounted;
+  sheepCounted++;
 }
 console.log("Zzzzzzzzzzz");
