9c21cf4cc2cf2690df26db675f84a00e9e73566e
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-11-14 14:57:19 +0200

    Update cyclesJs.js

--- cyclesJs.js
+++ cyclesJs.js
@@ -79,15 +79,15 @@ var output = "";
 
 for (var i = 0; i < input.length; i++) {
   if (input[i] === "a") {
-    console.log(output += 4);
+    output += 4;
   } else if (input[i] === "e") {
-    console.log(output += 3);
+    output += 3;
   } else if (input[i] === "i") {
-    console.log(output += 1);
+    output += 1;
   } else if (input[i] === "o") {
-    console.log(output += 0);
+    output += 0;
   } else {
-    console.log(output += input[i]);
+    output += input[i];
   }
 };
 console.log(output);
